f78ac28ec2352305612872446bb97a843fb4bb31
Mary Sharp <marysharp0721@gmail.com>
2024-11-08 15:37:32 +0000

    burger menu added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Head from 'next/head.js';
 import LandingPage from '../components/LandingPage.jsx';
+import BurgerMenu from '@/components/BurgerMenu.jsx';
 
 export default function Homepage() {
   return (
     <>
+      <nav>
+      <BurgerMenu/>
+      </nav>
+   
       <h1>Homepage</h1>
       <p>Welcome. Collect money. Upgrade your Gear. Defeat the Boss. Simple.</p>
       <LandingPage />
