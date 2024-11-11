b1a02bc5a09b4783029582040b1bf50288ee0d04
Sameer Kohli <skohli1508@gmail.com>
2024-11-11 14:19:04 +0000

    fixing shop again

--- src/components/ShopItems.jsx
+++ src/components/ShopItems.jsx
@@ -1,29 +1,71 @@
-import { db } from '@/utils/dbConnection';
+'use client';
+import { useEffect, useState } from 'react';
 
-export default async function ShopItems() {
-  let shopItems = [];
-  const shop = await db.query(
-    `SELECT id, cost, item, dps, health, description,sell_value, category FROM shop_table`
-  );
-  shopItems = shop.rows;
+export default function Shop({ count, setCount }) {
+  const [shopItems, setShopItems] = useState([]);
+  const [expandedItems, setExpandedItems] = useState({});
+  const [showShop, setShowShop] = useState(false);
 
-  console.log(shopItems, shop);
+  useEffect(() => {
+    async function fetchShopItems() {
+      const response = await fetch('http://localhost:3001/api/shop');
+      const data = await response.json();
+      setShopItems(data);
+    }
+    fetchShopItems();
+  }, []);
+
+  function buy(cost, item) {
+    if (count >= cost) {
+      setCount(count - cost);
+      alert(`You bought the ${item}`);
+    } else {
+      alert(`You do not have enough money to purchase the ${item}`);
+    }
+  }
+
+  function toggle(itemId) {
+    setExpandedItems((prev) => ({
+      ...prev,
+      [itemId]: !prev[itemId],
+    }));
+  }
+
+  const toggleShop = () => {
+    setShowShop((prev) => !prev);
+  };
+
   return (
     <div>
-      <p> Shop </p>
-      <ul className='shopLists'>
-        {shopItems.map((item) => (
-          <li key={item.id}>
-            <p>Item:{item.shopItem}</p>
-            <p>Cost:{item.cost}</p>
-            <p>Damage Per Second:{item.dps}</p>
-            <p>Health:{item.health}</p>
-            <p>Description:{item.description}</p>
-            <p>Sell Value:{item.sell_value}</p>
-            <p>Catergory:{item.category}</p>
-          </li>
-        ))}
-      </ul>
+      <h1 onClick={toggleShop} style={{ cursor: 'pointer' }}>
+        Shop
+      </h1>
+      {showShop && (
+        <ul className='shopLists'>
+          {shopItems.map((item) => (
+            <li key={item.id}>
+              <div
+                onClick={() => toggle(item.id)}
+                style={{ cursor: 'pointer' }}
+              >
+                <p>{item.item}</p>
+              </div>
+
+              {expandedItems[item.id] && (
+                <>
+                  <p>Cost: {item.cost}</p>
+                  <p>Damage Per Second: {item.dps}</p>
+                  <p>Health: {item.health}</p>
+                  <p>Description: {item.description}</p>
+                  <p>Sell Value: {item.sell_value}</p>
+                  <p>Category: {item.category}</p>
+                  <button onClick={() => buy(item.cost, item.item)}>Buy</button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
